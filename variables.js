f33d3df4ca003d3f9a9b18f98e277fee18291ff5
shubh <shubhamdixit863@gmail.com>
2022-11-23 21:59:47 +0530

    operators

--- variables.js
+++ variables.js
@@ -33,10 +33,10 @@ let shubham={firstName:"Shubham",age:22,address:"Delhi"}
 // difference between let and var and const
 
 // you can't redeclare let and const 
-var v=9;
-console.log(v)
-var v=89;
-console.log(v);  //works
+//var v=9;
+//console.log(v)
+//var v=89;
+//console.log(v);  //works
 //let  bn=99;
 //console.log(bn);
 //let bn=89;
@@ -48,4 +48,77 @@ console.log(v);  //works
 //console.log(cn);
 // Conclusion -->var can be redeclared but let and const can't be redeclared 
 
+// undefined is a primitive data type which is used as a default value of the variables 
+
+//var c;
+//let h;
+//console.log(c);
+//console.log(h);
+
+
+// Next difference Attachment to the window object -- Variables
+// variables declared with var get attached to the window object but variables declared with let and const dont
+
+//var vb=99000;
+//let hb=90;
+//const gh=99;
+//console.log(window.vb);// window is a global variable present in every browser
+//console.log(window.hb);
+//console.log(window.gh);
+
+// --->scopes --->
+
+/*
+
+// . ---Operators ----->
+// Arithmetic operators 
+// +
+var a1=89;
+var a2=90;
+var sum=a1+a2;
+console.log(sum);
+
+let s1="hello";
+let s2="world";
+let stringsum=s1+s2; //concatanates the string or joins them
+console.log(stringsum) //string output
+
+// -
+
+var difference=a2-a1;
+var stringdiffernce=s1-s2;
+var stringSum=a1+s1;
+console.log(stringSum);
+
+
+//console.log("String",stringdiffernce)
+console.log(difference)
+
+// * 
+let multiplication=a1*a2;
+console.log(multiplication)
+
+// / (division) ;
+let divide=a1/a2;
+console.log(divide);
+*/
+// % modulo -->it returns the remainder
+
+let gh=78;
+let vb=2;
+let q=gh/vb;
+let modulo=gh%vb;
+let smallerBybigger=vb%gh;
+console.log(smallerBybigger)
+//console.log(modulo);
+//console.log("quotient",q)
+
+// = Assignment 
+let hjjkk=9; // assignment 
+let bn=9;
+//bn=90; // assignment
+bn=90;
+console.log(hjjkk == bn ) // equality operator gives you the result in boolean
+
+// == equalit -- === (strict equality); which checks for the type as well -->
 
