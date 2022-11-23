6f549beee976568f901225d68e29fb20fa24d4ae
shubh <shubhamdixit863@gmail.com>
2022-11-23 20:50:10 +0530

    data types

---
+++ variables.js
@@ -0,0 +1,18 @@
+var age=20;  // variable creation with var keyword
+let name="shubham";
+const value=true;
+
+
+// different data types in js --
+//primitive data types -->basic datatypes or elementary  datatypes that any language offers
+
+// number ,string ,boolean ,undefined ,null ,symbol,bigint 
+
+// Non primitive -->User defined 
+// Object literals  ,arrays ,Map ,Set ,WeakMap
+// Arrays -- 
+let ageArray=[1,2,3,4,5,6]; // Array -- >is a collection of data (data can be or primitive type only or it can be combination of primitive and non primitive)
+
+// Object literal   // 
+let shubham={firstName:"Shubham",age:22,address:"Delhi"}
+
