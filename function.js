71fb400e1453568255cd9ea965547b9dbf9169cb
shubh <shubhamdixit863@gmail.com>
2022-11-24 20:57:22 +0530

    code

---
+++ function.js
@@ -0,0 +1,55 @@
+// function definition
+
+var k=9;
+///
+
+//return sum;
+// functions packages your logic and code separately -->such that you can selectively execute them
+
+function foo (){
+    // you put upall your code that you want to execute
+
+    var firstNum=1;
+var secondNum=2;
+var sum =firstNum+secondNum;
+
+
+return sum;
+   
+}
+// function call
+console.log(foo());
+
+
+// with help of function 
+
+function product(){
+    var p1=8;
+var p2=9;
+var productvalue=p1*p2;
+
+
+}
+//product()
+
+// next benefit is using reusability\
+
+
+function productofAll(q1,q2){ //parameteres
+  //  var q1=9;
+//var q2=89;
+var pro=q1*q2;
+console.log(pro);
+
+
+}
+productofAll(2,3);  // arguments
+productofAll(8,9);
+productofAll(7,8)
+
+
+
+// Write a function that calculates the sum of 2 numbers
+// product of two numbers
+// divison of two numbers
+// modulo of two numbers
