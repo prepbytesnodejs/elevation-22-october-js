e506ca7bf82c3dcc9454ec79c0b1ee71ac8720f3
shubh <shubhamdixit863@gmail.com>
2022-12-18 14:02:19 +0530

    execution context

---
+++ eexecutioncontext.js
@@ -0,0 +1,24 @@
+
+// eval execution context
+eval("let a=9;let b=10;let sum=a+b;console.log(a+b);function sumNum(){let a=9;let b=10;let sum=a+b;console.log(a+b);}")
+
+let a=9;
+let b=10;
+
+let sum=a+b;
+
+console.log(a+b);
+
+
+function sumNum(){
+
+  let a=9;
+let b=10;
+
+let sum=a+b;
+
+console.log(a+b);
+  
+}
+
+eval("let a=2+2 ;console.log(a)")
